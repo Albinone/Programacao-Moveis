c57dafff197d9cc165efa6ba57463eda2e3b8696
Albinone <jose.rei.albino@gmail.com>
2025-10-15 23:25:45 +0100

    Projecto Meterologia

--- app/src/main/java/com/example/metrologia/MainActivity.kt
+++ app/src/main/java/com/example/metrologia/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,9 +21,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,17 +44,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun TopBar(): Unit{
     Row(
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 15.dp, vertical = 25.dp ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 15.dp, vertical = 25.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
     ){
         Box(
             modifier = Modifier
-                    .size(size = 52.dp)
-                    .clip(CircleShape)
+                .size(size = 52.dp)
+                .clip(CircleShape)
                 .background(Color.White)
                 .padding(10.dp)
 
@@ -78,4 +84,23 @@ fun TopBar(): Unit{
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun FullComponent(): Unit {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(painter = painterResource(R.drawable._86449326_37c2256216_b),"asd",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxHeight().alpha(0.5f))
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            TopBar()
+        }
+
+    }
+    
+}
 
